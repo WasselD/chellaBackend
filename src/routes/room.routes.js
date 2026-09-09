c16aa7ae2f93f70cdcb08957d6162cd058901c78
Wassel Dridi <wasselforbusiness@gmail.com>
2026-09-09 23:24:51 +0100

    fix room router

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { 
   createRoom, 
-  getRooms 
+  getRoomByCode 
 } from '../controllers/room.controller.js';
 import { requireAuth } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.get('/', getRooms);
-
 
 router.post('/', requireAuth, createRoom);
+
+router.get('/code/:code', getRoomByCode);
 
 export default router;
